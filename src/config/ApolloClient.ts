c4a13ee8e58f9c65ed0aef582a214be32029fe07
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2026-01-07 20:25:28 +0530

    Base graphql setup

---
+++ src/config/ApolloClient.ts
@@ -0,0 +1,13 @@
+import { InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink } from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
